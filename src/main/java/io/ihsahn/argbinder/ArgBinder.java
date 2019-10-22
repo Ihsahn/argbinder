ba539a186faa200110e15aa5e48caf2d32727045
Ihsahn <Ihsahn@users.noreply.github.com>
2019-10-22 18:31:16 +0200

    Allow to bind simple spring properties of (possibly nested) classes (#2)

--- src/main/java/io/ihsahn/argbinder/ArgBinder.java
+++ src/main/java/io/ihsahn/argbinder/ArgBinder.java
@@ -1,4 +1,32 @@
 package io.ihsahn.argbinder;
 
+import org.apache.commons.beanutils.PropertyUtils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class ArgBinder {
+
+    private final Object target;
+
+    public ArgBinder(Object target) {
+        this.target = target;
+    }
+
+    public void parse(String[] args) {
+        Iterator<String> argsIterator = Arrays.asList(args).iterator();
+        while (argsIterator.hasNext()) {
+            String paramName = argsIterator.next();
+            if (argsIterator.hasNext()) {
+                String value = argsIterator.next();
+                try {
+                    PropertyUtils.setNestedProperty(target, paramName, value);
+                }  catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
 }
--- src/test/java/io/ihsahn/argbinder/ArgBinderTest.java
+++ src/test/java/io/ihsahn/argbinder/ArgBinderTest.java
@@ -1,7 +1,73 @@
 package io.ihsahn.argbinder;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArgBinderTest {
 
+    @Test
+    public void testFlatClassStringValues() {
+        FlatClassStringValues target = new FlatClassStringValues();
+        ArgBinder binder = new ArgBinder(target);
+        String[] args = {"name", "some name", "description", "some description"};
+        binder.parse(args);
+
+        assertEquals("some name", target.getName());
+        assertEquals("some description", target.getDescription());
+    }
+
+    @Test
+    public void testNestedClassStringValues() {
+        UpperLevelClassStringValues target = new UpperLevelClassStringValues();
+        ArgBinder binder = new ArgBinder(target);
+        String[] args = {"topLevelDescription", "top level description", "values.name", "some name", "values.description", "some description"};
+        binder.parse(args);
+
+        assertEquals("top level description", target.getTopLevelDescription());
+        assertEquals("some name", target.getValues().getName());
+        assertEquals("some description", target.getValues().getDescription());
+    }
+
+    public static class UpperLevelClassStringValues {
+        private String topLevelDescription;
+        private FlatClassStringValues values = new FlatClassStringValues();
+
+        public String getTopLevelDescription() {
+            return topLevelDescription;
+        }
+
+        public void setTopLevelDescription(String topLevelDescription) {
+            this.topLevelDescription = topLevelDescription;
+        }
+
+        public FlatClassStringValues getValues() {
+            return values;
+        }
+
+        public void setValues(FlatClassStringValues values) {
+            this.values = values;
+        }
+    }
+
+    public static class FlatClassStringValues {
+        private String name;
+        private String description;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    }
 }
