bdc24234b5fbba2932e9a748dbd2cbe7c5f7946b
Ihsahn <Ihsahn@users.noreply.github.com>
2019-11-27 20:54:20 +0100

    Add support for list fields (set at once) (#6)

--- src/test/java/io/ihsahn/argbinder/ArgBinderTest.java
+++ src/test/java/io/ihsahn/argbinder/ArgBinderTest.java
@@ -2,7 +2,7 @@ package io.ihsahn.argbinder;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ArgBinderTest {
 
